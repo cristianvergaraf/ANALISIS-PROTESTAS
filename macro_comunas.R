de14b309fe1ee52678445211f07d08fbf510f9c6
Cristian Vergara Fernández <cristianvergaraf@gmail.com>
2021-09-29 05:12:53 -0300

    limpieza de fichero
    Cambiamos menores, agregar comentarios y revisar código

--- macro_comunas.R
+++ macro_comunas.R
@@ -1,5 +1,8 @@
-####
-## Este program ejecuta las funciones que creamos 
+##
+## En este script vamos a utilizar la funcion macro_pais, que fue definida en el fichero macro_unidades.R, para generar a partir de la capa shape de todas las comunas de Chile
+## macro unidades que contempla una comuna central, y todas las comunas vecinas a esta. Los resultados son exportardos a un geopackage, el nombre de cada macrocomuna
+## incluye el código de la comuna central.
+##
 
 library(sp)
 library(sf)
@@ -19,9 +22,7 @@ chile <- st_read(dsn = ".", layer = "comunas")
 chile$COMUNA
 ########################################################
 
-macro_pais("8312","C:/projectos/dizzi/datos_espaciales/resultados" )
-
-##### Ahora debemos aplicar el calcola para todas las comunas y obtemos un shape para cada comuna. 
+##### Ahora aplicamos la función para todas las comunas y obtemos un shape para cada comuna, que es exportado a un geopackage creado en el directorio indicado.
 
 comunas <- as.character(chile$COMUNA)
 
