285d529ba497c00194d4c504e0576900ac42b16e
Cristian Vergara Fernández <cristianvergaraf@gmail.com>
2021-09-29 05:36:32 -0300

    comentario y limpieza

--- merge_csv_function.R
+++ merge_csv_function.R
@@ -1,5 +1,5 @@
 
-### Funcion obtenida de stackOverflow para leer todos los csv de un directorio y rbind todos ellos creando 
+### Funcion obtenida de stackOverflow para leer todos los csv de un directorio, y generar un fichero con toda la información (rbind) 
 ### un solo data.frame
 
 
@@ -9,6 +9,3 @@ load_data <- function(path) {
   do.call(rbind, tables)
 }
 
-protestas_comunas_chile_completo <- load_data("C:/projectos/dizzi/datos_espaciales/res_protestas_tiempo")
- 
-write.csv(protestas_comunas_chile_completo, file = "C:/projectos/dizzi/datos_espaciales/res_protestas_tiempo/protestas_comunas_chile_completo.csv")
